04c5ec4db14403dedd35df789584320b156f82a8
masudalimran <masudalimran92@gmail.com>
2022-03-06 18:36:07 +0600

    revert back

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import SinglePost from "./screens/SinglePost";
 import TopBar from "./components/topbar/TopBar";
-// import Home from "./screens/Home";
+import Home from "./screens/Home";
 import Write from "./screens/Write";
 import AboutUs from "./screens/AboutUs";
 import Blog from "./screens/Blog";
@@ -9,27 +9,23 @@ import ContactUs from "./screens/ContactUs";
 import Footer from "./components/Footer";
 import ProfilePage from "./screens/User/ProfilePage";
 import EditProfilePage from "./screens/User/EditProfilePage";
-import { lazy, Suspense } from "react";
-const Home = lazy(() => import("./screens/Home"));
 
 function App() {
   return (
     <>
       <TopBar />
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/">
-            <Route index element={<Home />} />
-            <Route path="/single-post/:id" element={<SinglePost />} />
-            <Route path="/about-us" element={<AboutUs />} />
-            <Route path="/contact" element={<ContactUs />} />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/write-post" element={<Write />} />
-            <Route path="/profile" element={<ProfilePage />} />
-            <Route path="/edit-profile" element={<EditProfilePage />} />
-          </Route>
-        </Routes>
-      </Suspense>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="/single-post/:id" element={<SinglePost />} />
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/contact" element={<ContactUs />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/write-post" element={<Write />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/edit-profile" element={<EditProfilePage />} />
+        </Route>
+      </Routes>
       <Footer />
     </>
   );
--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -1,14 +1,10 @@
-import { lazy, Suspense } from "react";
 import Body from "../components/body/Body";
-// import Header from "../components/header/Header";
-const Header = lazy(() => import("../components/header/Header"));
+import Header from "../components/header/Header";
 
 export default function Home() {
   return (
     <>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Header />
-      </Suspense>
+      <Header />
       <Body />
     </>
   );
