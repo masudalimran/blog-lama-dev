c2c8a4c12bd2a109e456f179d7e0e1603cc29049
masudalimran <masudalimran92@gmail.com>
2022-03-06 18:31:35 +0600

    update

--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -1,10 +1,14 @@
+import { lazy, Suspense } from "react";
 import Body from "../components/body/Body";
-import Header from "../components/header/Header";
+// import Header from "../components/header/Header";
+const Header = lazy(() => import("../components/header/Header"));
 
 export default function Home() {
   return (
     <>
-      <Header />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Header />
+      </Suspense>
       <Body />
     </>
   );
